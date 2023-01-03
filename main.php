3e5f1e168060dbaf466c64496a4a0e2a4c566671
SP7TECH <spandey247789@gmail.com>
2023-01-03 11:29:42 +0530

    Main file frontend

--- main.php
+++ main.php
@@ -1,8 +1,22 @@
-<head>
-<link rel="shortcut icon" href="/assets/favicon.ico">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-<link rel="stylesheet" href="app.css">
-</head>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>As Trends Ecommerce</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="shortcut icon" href="/assets/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <script src="https://kit.fontawesome.com/9b90885e16.js" crossorigin="anonymous"></script>
+    
+    <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+    crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="app.css">
+  </head>
 <?php
 
 // session_start();
@@ -14,59 +28,28 @@
 //}
 ?>
 
-<header class=nav>
-     <div class="container">
-		<div class="row">
-		    <div class="company-name">
-			   <h2>Welcome !!! </h2>
-			</div>
-			
-			<ul class="main-nav">
-				<li><a href=cust.php>Customer</a></li>
-				<li><a href=prod.php>Product </a></li>
-				<li><a href=shop_listing.php>Shopping </a></li>
-				<li><a href=logout.php>Logout </a> </li>
-			</ul>
-		</div>
-	 </div>
-
-</header>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title>As Trends Ecommerce</title>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&family=Ubuntu&display=swap"
-      rel="stylesheet"/>
-
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
-      crossorigin="anonymous"
-    />
-
-    <link rel="stylesheet" href="style.css" />
-
-    <script
-      src="https://kit.fontawesome.com/9b90885e16.js"
-      crossorigin="anonymous"
-    ></script>
-
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-      crossorigin="anonymous"
-    ></script>
-  </head>
   <body>
+    <header class=nav>
+     <div class="container">
+        <div class="row">
+            <div class="company-name">
+            <h2>Welcome !!! </h2>
+          </div>
+          
+          <ul class="main-nav">
+            <li><a href=cust.php>Customer</a></li>
+            <li><a href=prod.php>Product </a></li>
+            <li><a href=shop_listing.php>Shopping </a></li>
+            <li><a href=logout.php>Logout </a> </li>
+          </ul>
+        </div>
+      </div>
+
+    </header>
     <section id="title">
-      <div class="container-fluid">
+      <div class="container">
         <!-- Nav Bar -->
-        <nav class="navbar navbar-expand-lg navbar-dark font:dark fixed-top">
+        <nav class="navbar navbar-expand-lg text-dark">
           <a class="navbar-brand" href="#"><h1><b> As TRENDS </b></h1></a>
           <button
             class="navbar-toggler"
@@ -102,35 +85,28 @@
             </ul>
           </div>
         </nav>
+    </div>
 
 
 
     <!-- Testimonials -->
 
     <section id="testimonials">
-      <div
-        id="carouselExampleControls"
-        class="carousel slide"
-        data-bs-ride="carousel"
-      >
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="4000">
-            
             <img
               class="testimonials-image"
-              src="1.jpeg"
+              src="1.jpg"
               alt="profile"
             />
-            
           </div>
           <div class="carousel-item" data-bs-interval="4000">
-           
             <img
               class="testimonials-image"
-              src="2.jpeg"
+              src="2.jpg"
               alt="kkprofile"
             />
-           
           </div>
         </div>
         <button
@@ -153,53 +129,45 @@
         </button>
       </div>
     </section>
-    <br>
-    <br>
-    <div class="row1">
-        <div class="column1">
-          <img src="3.jpeg" alt="Snow" class="pic1">
+    
+    <section class="categories">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-6">
+            <img src="3.jpg" alt="Snow" class="pic1">
+          </div>
+          <div class="col-md-6 col-lg-6">
+            <img src="5.jpg" alt="Forest" class="pic2">
+          </div>   
         </div>
-        <div class="column1">
-          <img src="4.jpeg" alt="Forest" class="pic2">
-        </div>   
-    </div>
-
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <h1 class="new"> NEW ARRIVALS</h1>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <div class="row1">
-      <div class="column1">
-        <img src="5.jpeg" alt="Snow" style="width:100%" height="500px">
       </div>
-      <div class="column1">
-        <img src="6.jpeg" alt="Forest" style="width:100%" height="500px">
-      </div>   
-      <div class="column1">
-        <img src="7.jpeg" alt="Forest" style="width:100%" height="500px">
-      </div>  
-    </div>
+    </section>
 
-    <br>
-    <br>
-    <br>
-    <h2 class="history">CLOTHES AVAILABLE</h2>
-    <p class="his"></p>
+    <section class="new-arrivals">
+      <div class="container">
+        <div class="row">
+          <h1> NEW ARRIVALS</h1>
+
+            <div class="col-md-4 col-lg-4">
+              <img src="5.jpg" alt="Snow" style="width:100%" height="500px">
+            </div>
+            <div class="col-md-4 col-lg-4">
+              <img src="6.jpg" alt="Forest" style="width:100%" height="500px">
+            </div>   
+            <div class="col-md-4 col-lg-4">
+              <img src="7.jpg" alt="Forest" style="width:100%" height="500px">
+            </div>  
+        </div>
+      </div>
+    </section>
      
   </body>
   
     
-  <footer>
+  <footer class="web-footer">
     <div class="container">
-        <div class="left section">
+        <div class="row">
+          <div class="col-md-4 col-lg-4">
             <h2>About Us</h2>
             <div class="content">
                 <p>As Trends is a clothing website where we intend to sell premium quality clothing at a reasonable price.</p>
@@ -208,11 +176,10 @@
                 <a href="https://instagram.com/multiwebpress"><span class="fab fa-instagram"></span></a>
                 <a href="https://www.youtube.com/channel/UCvB-4cVzkhvu4TkTy1hs9zA"><span class="fab fa-youtube"></span></a>
             </div>
-            </div>
         </div>
-
-        <div class="center section">
-            <h2>Contact Us</h2>
+          </div>
+          <div class="col-md-4 col-lg-4">
+          <h2>Contact Us</h2>
             <div class="content">
                 <div class="place">
                     <span class="fas fa-map-marker-alt"></span>
@@ -227,10 +194,9 @@
                     <span class="text">astrends11@gmail.com</span>
                 </div>
             </div>
-        </div>
-
-        <div class="right section">
-            <h2>Subscribe</h2>
+          </div>
+          <div class="col-md-4 col-lg-4">
+          <h2>Subscribe</h2>
             <div class="content">
                 <form action="#">
                     <div class="email">
@@ -238,13 +204,12 @@
                     </div>
                     <input type="email" required>
                     <div class="btn">
-                        <button type="submit">SUBSCRIBE</button>
+                        <button type="submit" class="btn btn-primary">SUBSCRIBE</button>
                     </div>
                 </form>
             </div>
+          </div>
         </div>
-    </div>
-
     <div class="footer-bottom">
         <center>
             <div><span>Powered By As Trends<a href="#">© 2022 | All Rights Reserved </a></span></div>
